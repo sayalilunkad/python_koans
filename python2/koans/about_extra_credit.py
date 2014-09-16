c2cb34bdac61be2820320751bcb9177ef0a17eaf
Sayali Lunkad <sayali.92720@gmail.com>
2014-09-16 22:49:00 +0530

    extra

--- python2/koans/about_extra_credit.py
+++ python2/koans/about_extra_credit.py
@@ -12,9 +12,53 @@
 
 from runner.koan import *
 
+class DiceSet(object):
+    def __init__(self):
+        self._values = None
+
+    @property
+    def values(self):
+        return self._values
+
+    def roll(self, n):
+        # Needs implementing!
+        self._values = []
+        for r in range(0, n):
+            self._values.append(random.randint(1, 6))
+
+
+def score(dice):
+    '''
+    This calculates the score for one roll of dice according to the rules below:
+    Three 1's => 1000 points
+    Three 6's =>  600 points
+    Three 5's =>  500 points
+    Three 4's =>  400 points
+    Three 3's =>  300 points
+    Three 2's =>  200 points
+    One   1   =>  100 points
+    One   5   =>   50 points
+    '''
+    numbers = {}.fromkeys(range(1, 7), 0)
+
+    for d in dice:
+        numbers[d] += 1
+    sum = (numbers[1] // 3) * 1000
+
+    sum += (numbers[1] % 3) * 100
+
+    sum += (numbers[5] % 3) * 50
+
+    for r in range(2, 7):
+        sum += (numbers[r] // 3) * 100 * r
+    return sum
+
 
 class AboutExtraCredit(Koan):
     # Write tests here. If you need extra test classes add them to the
     # test suite in runner/path_to_enlightenment.py
     def test_extra_credit_task(self):
+        pass
+    def test_score():
+        self.assertEqual(score([5, 1, 3, 4, 1]), 250)
         pass
